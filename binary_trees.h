680c18cff1c3bd8c3cd4f2468d2dfaf88d3219cc
mosesgitonga <infosec947@gmail.com>
2023-07-31 21:02:42 +0300

    task 0

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,12 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+
+
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -16,4 +23,9 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
+typedef struct binary_tree_s binary_tree_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* _BINARY_TREES_H_ */
