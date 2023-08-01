5d6994517798b45f3affb80811ef2b33e1729685
mosesgitonga <infosec947@gmail.com>
2023-08-01 17:56:41 +0300

    task 1 trial

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,11 +8,12 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newnode = NULL;
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
-	binary_tree_t *newnode = binary_tree_node(parent, value);
+	newnode = binary_tree_node(parent, value);
 
 	if (newnode == NULL)
 	{
