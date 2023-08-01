7d6eb86b180ee72f98303af1d597ba59d3a39ce1
mosesgitonga <infosec947@gmail.com>
2023-08-01 14:12:59 +0300

    trial

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,7 +4,7 @@
  *binary_tree_insert_left - insert the left of a node
  *@parent: node
  *@value: value of newnode
-  *Return: newnode
+ *Return: newnode
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -21,7 +21,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	newnode->left = parent->left;
 	if (parent->left != NULL)
+	{
 		parent->left->parent = newnode;
+	}
 	parent->left =  newnode;
 	return (newnode);
 }
+
