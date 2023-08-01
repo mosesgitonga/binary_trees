1548a69cf9350c4e530ba1ec56211f17f634ef5f
mosesgitonga <infosec947@gmail.com>
2023-08-01 10:49:37 +0300

    done with task 2

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_insert_left - insert the left of a node
+ *@parent: node
+ *@value: value of newnode
+  *Return: newnode
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	binary_tree_t *newnode = binary_tree_node(parent, value);
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+
+	newnode->left = parent->left;
+	if (parent->left != NULL)
+		parent->left->parent = newnode;
+
+
+	parent->left =  newnode;
+	return (newnode);
+
+}
