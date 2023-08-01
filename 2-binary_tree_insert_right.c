6256549a1fb5ca9c8420549897b0d4687578dfbb
mosesgitonga <infosec947@gmail.com>
2023-08-01 17:58:50 +0300

    fixing memory leak

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,12 +8,13 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newnode = NULL;
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
-	binary_tree_t *newnode = binary_tree_node(parent, value);
+	newnode = binary_tree_node(parent, value);
 
 	if (newnode == NULL)
 	{
