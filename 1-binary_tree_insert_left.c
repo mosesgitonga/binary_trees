6aa429ed22207a87d6e9d964bf6b30c3fdaf7eb5
mosesgitonga <infosec947@gmail.com>
2023-08-01 14:05:09 +0300

    remove betty issue

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,8 +22,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	newnode->left = parent->left;
 	if (parent->left != NULL)
 		parent->left->parent = newnode;
-
-
 	parent->left =  newnode;
 	return (newnode);
 }
