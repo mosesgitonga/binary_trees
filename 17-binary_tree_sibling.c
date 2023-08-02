29b8681fb47dbd809589504a6f264f2985cb43bd
mosesgitonga <infosec947@gmail.com>
2023-08-02 11:56:42 +0300

    betty problem on task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/*
+ * binary_tree_sibling - Finding sibling of a node.
+ * @node: curr node
+ *
+ * Return: sibling node
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (node->parent->left == node)
+	{
+		return (node->parent->right);
+	}
+	if (node->parent->right == node)
+	{
+		return (node->parent->left);
+	}
+
+	return (NULL);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -39,7 +39,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
-
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 
 
