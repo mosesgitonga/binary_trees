140e5d44b0ab40a0387ad6713cf853222b8be10c
mosesgitonga <infosec947@gmail.com>
2023-08-02 12:20:13 +0300

    fixed betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,4 +1,10 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_uncle - search for the uncle of a node
+ * @node: curr node
+ * Return: uncle of a node else NULL
+ */
+
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
