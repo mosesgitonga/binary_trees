00bd6434711d1f8f88a5dcf432586f68e57f9252
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2023-08-02 04:01:10 +0300

    Updates on Binary_trees

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - A function that counts the nodes with at least 1 child in a binary tree
+ *
+ * @tree: Pointer to the root node
+ * Return: Number of Nodes or 0 if Null
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    /*Check The tree and the left and right nodes*/
+    /*If either one is Null return 0*/
+    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+        return (0);
+
+    return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Helper function to get height
+ * of a binary tree
+ * @tree: Pointer to root
+ * Return: Height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height, right_height;
+
+    if (tree == NULL)
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
+ *binary_tree_balance - A function that gives the
+ *balance of the tree
+ *@tree: Pointer to root
+ *Return: Balanced tree
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    /* Check if the tree is null */
+    if (tree == NULL)
+        return (0);
+
+    /**
+     * Return the subtraction value of the left and
+     * right subtrees to determine if the tree
+     * is balanced
+     */
+    return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
