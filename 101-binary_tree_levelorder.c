b56c63dcc8b30334ce1a1efe4b85db88e9a99d7f
mosesgitonga <infosec947@gmail.com>
2023-08-03 14:28:07 +0300

    level order

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_levelorder - traverse through by level order
+ * @tree:  tree to traverse
+ * @func: function to print
+ * Return: NULL or elements in tree
+ */
+
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	binary_tree_t **queue = malloc(sizeof(binary_tree_t) * 1024);
+
+	if (queue == NULL)
+	{
+		return;
+	}
+
+	int rear = 0;
+	int front = 0;
+
+	queue[rear++] = (binary_tree_t *) tree;
+
+	while (front < rear)
+	{
+		binary_tree_t *current = queue[front++];
+
+		if (current == NULL)
+		{
+			return;
+		}
+		func(current->n);
+
+		queue[rear++] = current->left;
+		queue[rear++] = current->right;
+	}
+
+	free(queue);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -44,6 +44,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 
 
